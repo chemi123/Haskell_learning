21c1ffa69c0080e2f51c9d953355442a4337521c
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-12-13 08:01:51 +0900

    new scripts

--- programs/chapter_6.hs
+++ programs/chapter_6.hs
@@ -1,4 +1,14 @@
+import Data.Char
 import Data.List
+import Geometry
 
 wordsNum :: String -> [(String, Int)]
 wordsNum = map (\ws -> (head ws, length ws)) . group . sort . words
+
+
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1..]
