8614acc8f4fbdadd83b7e0d99d776d86031c4b45
Yuichi Tanimoto <ytanimot@yahoo-corp.jp>
2018-02-27 00:05:18 +0900

    update chapter 13

--- programs/chapter_13.hs
+++ programs/chapter_13.hs
@@ -6,3 +6,19 @@ Nothing `applyMaybe` f = Nothing
 --  (>>=) :: Maybe a -> (a -> Maybe b) -> Maybe b
 --  Nothing >>= f = Nothing
 --  (Just x) >>= f = f x
+
+
+type Birds = Int
+type Pole = (Birds, Birds)
+
+
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+  | abs ((left + n) - right) < 4 = Just (left + n, right)
+  | otherwise                    = Nothing
+
+
+landRight:: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+  | abs (left - abs (right + n)) < 4 = Just (left, right + n)
+  | otherwise                        = Nothing
